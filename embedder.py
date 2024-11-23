3709aaf598af5b79c3633f3e454d6786f413307f
Galich6 <103468510+Galich6@users.noreply.github.com>
2024-11-23 04:54:22 -0800

    Update embedder.py

--- embedder.py
+++ embedder.py
@@ -32,4 +32,42 @@ with open(payload_path, "r") as file:
 image = Image.open(img_path)
 rgb_image = image.convert("RGB").load()
 
+# Embed payload into image 
+payload_index = 0
+width, height = image.size
 
+# Number of bits needed to embed/Number of bits in the image
+total_bits = len(payload)
+total_pixels = width * height * 3 # RGB channels
+
+# Ensure payload fits inside image
+if total_bits > total_pixels:
+    raise ValueError("Image is too small for payload")
+
+# for loop: loop over the pixels
+for y in range(height):
+    for x in range(width):
+        # Embedding bits into different channels red, green, blue by modifying LSB
+        r, g, b = rgb_image[x, y] # current value of RGB pixel
+        if payload_index < len(payload):
+            r = (r & 0xFE) | payload[payload_index] # modifying LSB of red channel
+            payload_index += 1
+        if payload_index < len(payload):
+            g = (g & 0xFE) | payload[payload_index] # moddifying LSB of green channel
+            payload_index += 1
+        if payload_index < len(payload):
+            b = (b & 0XFE) | payload[payload_index] # modifying LSB of blue channel
+            payload_index +=1
+        
+        rgb_image[x, y] = (r, g, b) # update pixel with the modified red, green, blue channels
+
+        if payload_index >= len(payload):
+            break # if there is no more bits left to embed
+    if payload_index >= len(payload):
+        break # if all bit have been embedded
+
+# save to tainted image
+image.save(out_path)
+
+# print successful embed
+print(f"Successfully embedded into {out_path}")

